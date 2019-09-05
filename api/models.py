654edfa0b3412682fa4a71beae33985eecaf39d6
kwonjae lee <kwonjae.2@navercorp.com>
2019-09-05 17:37:37 +0900

    Change DB to mongoDB with mongoengine

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from mongoengine import Document, StringField, EmailField, ReferenceField, ListField, DictField
 
-# Create your models here.
+
+class User(Document):
+    username = StringField(max_length=30)
+    email = EmailField(max_length=30)
+    friends = ListField(ReferenceField('self'))
+    extra = DictField()
+
+class Post(Document):
+    title = StringField(max_length=30)
+    body = StringField(max_length=1024)
+    author = StringField(max_length=30)
--- wayne/urls.py
+++ wayne/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', views.UserViewSet, base_name='users')
+router.register(r'posts', views.PostViewSet, base_name='posts')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
